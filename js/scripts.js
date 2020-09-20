354c5417fdf16ef0a79517e97c239bf524e2b607
Trevor2492 <69220176+Trevor2492@users.noreply.github.com>
2020-09-20 17:16:04 -0500

    Added logic to display the Pokemon names and sizes through a loop and added some styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 let pokemonList = [
   {name: 'Pikachu', height: '1', types: ['Electric', 'Fighting']},
   {name: 'Charmander', height: '2', types: ['Fire', 'Fighting']},
-  {name: 'Bulbasaur', height: '3', types: ['Grass', 'Fighting']}
+  {name: 'Bulbasaur', height: '3', types: ['Grass', 'Fighting']},
+  {name: 'Squirtle', height: '1.5', types: ['Water', 'Fighting']}
 ];
+
+for (var i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height <= '2') {
+    document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') ' + '<br>');
+  } else {
+    document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') ' + '- Wow, that\s big!' + '<br>');
+  }
+}
