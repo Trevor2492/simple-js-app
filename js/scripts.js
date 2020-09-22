e7655823374712cda9837e19389b25b6ea238de3
Trevor2492 <69220176+Trevor2492@users.noreply.github.com>
2020-09-22 14:48:25 -0500

    updated the Pokemon Array
    -changed the old array to be inside an IIFE
    -changed the "for" loop to a "forEach" loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,32 @@
-let pokemonList = [
-  {name: 'Pikachu', height: '1', types: ['Electric', 'Fighting']},
-  {name: 'Charmander', height: '2', types: ['Fire', 'Fighting']},
-  {name: 'Bulbasaur', height: '3', types: ['Grass', 'Fighting']},
-  {name: 'Squirtle', height: '1.5', types: ['Water', 'Fighting']}
-];
+//This defines an array of objects. The objects contain information for pokemon
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Pikachu', height: '1', types: ['Electric', 'Fighting']},
+    {name: 'Charmander', height: '2', types: ['Fire', 'Fighting']},
+    {name: 'Bulbasaur', height: '3', types: ['Grass', 'Fighting']},
+    {name: 'Squirtle', height: '1.5', types: ['Water', 'Fighting']}
+  ];
 
-for (var i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height <= '2') {
-    document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') ' + '<br>');
-  } else {
-    document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') ' + '- Wow, that\s big!' + '<br>');
+  function add(pokemon) {
+  pokemonList.push(pokemon);
   }
-}
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+//This is a loop that displays the contents of the array to the browser
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height <= '2') {
+    document.write(pokemon.name + ' (Height: ' + pokemon.height + ')' + '<br>');
+  }
+  else {
+    document.write(pokemon.name + ' (Height: ' + pokemon.height + ')' + '- Wow, that\'s big!' + '<br>');
+  }
+});
